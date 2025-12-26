cdce01256e297b89633004c767b93d80cece9650
edson66 <edsonulissesr1@gmail.com>
2025-12-26 02:25:17 -0300

    estrutura inicial

---
+++ api/src/test/java/com/nordeste/livraria/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nordeste.livraria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
