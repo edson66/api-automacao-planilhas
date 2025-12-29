4dcb5cf5adf8af920656942b3fc43d8de1e2adbd
edson66 <edsonulissesr1@gmail.com>
2025-12-29 18:11:21 -0300

    config para abrir navegador

---
+++ src/main/java/com/nordeste/livraria/api/nevagadorConfig/NavegadorLauncher.java
@@ -0,0 +1,25 @@
+package com.nordeste.livraria.api.nevagadorConfig;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class NavegadorLauncher {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void abrirNavegador() {
+        System.setProperty("java.awt.headless", "false");
+
+        String url = "http://localhost:8080";
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            // Comando para abrir navegador padrão no Windows
+            runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
